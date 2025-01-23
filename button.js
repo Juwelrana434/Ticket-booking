36ccb9dad0d15df47c507e6221be33cd69ad35fc
Juwelrana434 <jewel.rana434@gmail.com>
2025-01-23 21:01:47 +0600

    first commit

---
+++ button.js
@@ -0,0 +1,49 @@
+
+var button = document.getElementById("btna");
+button.addEventListener("click", function() {
+    button.style.backgroundColor = "green";
+    button.disabled = true;
+    
+});
+var buttonTwo = document.getElementById("btnb");
+buttonTwo.addEventListener("click", function() {
+    buttonTwo.style.backgroundColor = "green";
+    buttonTwo.disabled = true;
+    
+});
+var buttonc = document.getElementById("btnc");
+buttonc.addEventListener("click", function() {
+    buttonc.style.backgroundColor = "green";
+    buttonc.disabled = true;
+    
+});
+var buttond = document.getElementById("btnd");
+buttond.addEventListener("click", function() {
+    buttond.style.backgroundColor = "green";
+    buttond.disabled = true;
+    
+});
+var buttone = document.getElementById("btne");
+buttone.addEventListener("click", function() {
+    buttone.style.backgroundColor = "green";
+    buttone.disabled = true;
+    
+});
+var buttonf = document.getElementById("btnf");
+buttonf.addEventListener("click", function() {
+    buttonf.style.backgroundColor = "green";
+    buttonf.disabled = true;
+    
+});
+var buttong = document.getElementById("btng");
+buttong.addEventListener("click", function() {
+    buttong.style.backgroundColor = "green";
+    buttong.disabled = true;
+    
+});
+var buttonh = document.getElementById("btnh");
+buttonh.addEventListener("click", function() {
+    buttonh.style.backgroundColor = "green";
+    buttonh.disabled = true;
+    
+});
